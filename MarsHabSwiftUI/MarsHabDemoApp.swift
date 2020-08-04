34b822905723d3d570321e6d4ec376a23fbffcbc
Jim Hillhouse <jdhouse4@me.com>
2020-08-04 15:51:04 -0500

    Updated to Remove AppDelegate and SceneDelegate files

---
+++ MarsHabSwiftUI/MarsHabDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MarsHabDemoApp.swift
+//  MarsHabSwiftUI
+//
+//  Created by James Hillhouse IV on 8/4/20.
+//  Copyright © 2020 PortableFrontier. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+@main
+struct MarsHabDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
